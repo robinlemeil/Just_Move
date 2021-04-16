9efef52b12a0e6cf7db22a69edeca79667cda69c
robinlemeil <81814306+robinlemeil@users.noreply.github.com>
2021-04-16 16:29:02 +0200

    Initial commit

---
+++ app/src/main/java/fr/robguju/just_move/Page_Principale.java
@@ -0,0 +1,14 @@
+package fr.robguju.just_move;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Page_Principale extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page__principale);
+    }
+}
