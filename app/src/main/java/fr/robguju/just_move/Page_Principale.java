e48d1543ded5d2751e3589fa449b4f1559aa3835
robinlemeil <81814306+robinlemeil@users.noreply.github.com>
2021-05-07 12:30:55 +0200

    Ajout Boutons Accueil et Bouton Routine + lien nouvelle activity routine

--- app/src/main/java/fr/robguju/just_move/Page_Principale.java
+++ app/src/main/java/fr/robguju/just_move/Page_Principale.java
@@ -2,22 +2,45 @@ package fr.robguju.just_move;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class Page_Principale extends AppCompatActivity implements
         AdapterView.OnItemSelectedListener{
 
-    String[] type = { "Video", "Article", "Audio"};
+    //declaration objects
+    private Button onglet_accueil;
+    private Button onglet_routine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page__principale);
+
+        onglet_accueil = findViewById(R.id.id_onglet_ressource);
+        onglet_routine = findViewById(R.id.id_onglet_routine);
+
+        onglet_accueil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Page_Principale.this, Page_Principale.class));
+                }
+            }
+            );
+
+        onglet_routine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Page_Principale.this, Page_Routine.class));
+                 }
+             }
+             );
 
         //creation du spinner
         Spinner spinner = (Spinner) findViewById(R.id.id_type);
@@ -31,11 +54,10 @@ public class Page_Principale extends AppCompatActivity implements
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String text = parent.getItemAtPosition(position).toString();
-        Toast.makeText(getApplicationContext(),text , Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),text , Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-
     }
 }
