06dd9e35fe5558f5d5bdb8c83db2fd03c88a5765
Gu230 <gurvan.lecalve@gmail.com>
2021-05-07 16:54:07 +0200

    First part of settings with some idea, everything need to be debate and rework too adapte it with other part of app

--- app/src/main/java/fr/robguju/just_move/UserPage.java
+++ app/src/main/java/fr/robguju/just_move/UserPage.java
@@ -2,13 +2,31 @@ package fr.robguju.just_move;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserPage extends AppCompatActivity {
+
+    private Button eSettings;
+    private Button eFavoris;
+    private Button eTutoriels;
+    private Button eDeconnexion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_page);
+        eSettings = findViewById(R.id.button_Utilisateur);
+        eFavoris = findViewById(R.id.button_Favoris);
+        eTutoriels = findViewById(R.id.button_Tutoriels);
+        eDeconnexion = findViewById(R.id.button_Deconexion);
+
+        eSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, UserSettings.class));
+            }
     }
 }
