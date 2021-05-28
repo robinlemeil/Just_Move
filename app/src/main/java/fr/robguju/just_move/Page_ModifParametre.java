ce6e5d1e8316ce6321b9c217f2e82834b549c210
Gu230 <gurvan.lecalve@gmail.com>
2021-05-28 16:56:38 +0200

    Fin logique modifs valeurs utilisateurs.

--- app/src/main/java/fr/robguju/just_move/Page_ModifParametre.java
+++ app/src/main/java/fr/robguju/just_move/Page_ModifParametre.java
@@ -9,8 +9,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -22,15 +24,17 @@ public class Page_ModifParametre extends AppCompatActivity implements View.OnCli
     private EditText id_poids, id_temps_bureau, id_mail, id_password;
     private Button id_enregistrer;
     private DatabaseReference mDatabase;
+    private FirebaseUser currentUser;
+    private String iduser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page__modif_parametre);
 
-        Auth = FirebaseAuth.getInstance();
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-
+        currentUser = Auth.getInstance().getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference("Utilisateurs");
+        iduser =currentUser.getUid();
         id_logo = (ImageView) findViewById(R.id.id_logo);
         id_prenom = (EditText) findViewById(R.id.prenom);
         id_nom = (EditText) findViewById(R.id.nom);
@@ -38,8 +42,8 @@ public class Page_ModifParametre extends AppCompatActivity implements View.OnCli
         id_taille = (EditText) findViewById(R.id.taille);
         id_poids = (EditText) findViewById(R.id.poids);
         id_temps_bureau = (EditText) findViewById(R.id.temps_bureau);
-        id_mail = (EditText) findViewById(R.id.mail);
-        id_password = (EditText) findViewById(R.id.password);
+        /*id_mail = (EditText) findViewById(R.id.mail);
+        id_password = (EditText) findViewById(R.id.password);*/
         id_enregistrer = (Button) findViewById(R.id.enregistrer);
 
         id_logo.setOnClickListener(this);
@@ -51,62 +55,54 @@ public class Page_ModifParametre extends AppCompatActivity implements View.OnCli
         switch (v.getId()) {
             case R.id.enregistrer:
                 modif_compte();
+
         }
     }
 
     private void modif_compte() {
 
         String mprenom = id_prenom.getText().toString().trim();
-        String nom = id_nom.getText().toString().trim();
-        String age = id_age.getText().toString();
-        String taille = id_taille.getText().toString();
-        String poids = id_poids.getText().toString();
-        String temps = id_temps_bureau.getText().toString();
-        String mail = id_mail.getText().toString().trim();
-        String password = id_password.getText().toString().trim();
+        String mnom = id_nom.getText().toString().trim();
+        String mage = id_age.getText().toString();
+        String mtaille = id_taille.getText().toString();
+        String mpoids = id_poids.getText().toString();
+        String mtemps = id_temps_bureau.getText().toString();
+        /*String mmail = id_mail.getText().toString().trim();
+        String mpassword = id_password.getText().toString().trim();*/
 
         if (mprenom.length()>0) {
-            mDatabase.child("Utilasateurs").child("username").setValue(mprenom);
-            return;
+            mDatabase.child(iduser).child("prenom").setValue(mprenom);
+
         }
-        if (nom.isEmpty()) {
-            id_nom.setError("Nom manquant");
-            id_nom.requestFocus();
-            return;
-        }
-        if (!Patterns.EMAIL_ADDRESS.matcher(mail).matches()) {
-            id_mail.setError("Adresse mail incorrecte");
-            id_mail.requestFocus();
-            return;
-        }
-        if (password.isEmpty()) {
-            id_password.setError("Mot de passe manquant");
-            id_password.requestFocus();
-            return;
+        if (mnom.length()>0) {
+            mDatabase.child(iduser).child("nom").setValue(mnom);
+
         }
 
-        if (age.isEmpty()) {
-            id_age.setError("Age manquant");
-            id_age.requestFocus();
+        if (mage.length()>0) {
+            mDatabase.child(iduser).child("age").setValue(mage);
         }
 
 
-        if (taille.isEmpty()) {
-            id_taille.setError("Taille manquante");
-            id_taille.requestFocus();
+        if (mtaille.length()>0) {
+            mDatabase.child(iduser).child("taille").setValue(mtaille);
         }
 
 
-        if (poids.isEmpty()) {
-            id_poids.setError("Poids manquant");
-            id_poids.requestFocus();
+        if (mpoids.length()>0) {
+            mDatabase.child(iduser).child("poids").setValue(mpoids);
         }
 
 
-        if (temps.isEmpty()) {
-            id_temps_bureau.setError("Temps au bureau manquant");
-            id_temps_bureau.requestFocus();
+        if (mtemps.length()>0) {
+            mDatabase.child(iduser).child("temps_bureau").setValue(mtemps);
         }
+        if(true){
+            Toast.makeText(Page_ModifParametre.this,"Informations changees !",Toast.LENGTH_LONG).show();
+            startActivity(new Intent(Page_ModifParametre.this, Page_Principale.class));
+        }
+
+
 
 
     }
